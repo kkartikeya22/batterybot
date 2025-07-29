1d857bb20458a603ab2fe0bce3768343c438aed5
Kartikeya Katiyar <kkartikeya22@iitk.ac.in>
2025-07-29 10:37:27 +0530

    new

--- config/gcpCredentials.js
+++ config/gcpCredentials.js
@@ -3,7 +3,7 @@ export const gcpCredentials = {
   type: process.env.GCP_TYPE,
   project_id: process.env.GCP_PROJECT_ID,
   private_key_id: process.env.GCP_PRIVATE_KEY_ID,
-  private_key: process.env.GCP_PRIVATE_KEY?.replace(/\\n/g, '\n'), // Fix newlines
+  private_key: process.env.GCP_PRIVATE_KEY,
   client_email: process.env.GCP_CLIENT_EMAIL,
   client_id: process.env.GCP_CLIENT_ID,
   auth_uri: process.env.GCP_AUTH_URI,
